803cc9b1ed18ae5dababf530c37405ac8da6110f
martin-ngigi <martinwainaina001@gmail.com>
2022-12-12 12:38:15 +0300

    Added Department filament-resource

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -12,6 +12,12 @@ use Filament\Resources\Table;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Select;
+
+
 
 class DepartmentResource extends Resource
 {
@@ -23,7 +29,9 @@ class DepartmentResource extends Resource
     {
         return $form
             ->schema([
-                //
+                //https://filamentphp.com/docs/2.x/forms/layout#card
+                TextInput::make('name')
+
             ]);
     }
 
@@ -32,6 +40,11 @@ class DepartmentResource extends Resource
         return $table
             ->columns([
                 //
+                //https://filamentphp.com/docs/2.x/tables/columns/text
+                TextColumn::make('id')->sortable()->searchable(), //one can search
+                TextColumn::make('name')->sortable()->searchable(), //one can search
+                TextColumn::make('created_at')->dateTime()
+
             ])
             ->filters([
                 //
@@ -43,14 +56,14 @@ class DepartmentResource extends Resource
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -58,5 +71,5 @@ class DepartmentResource extends Resource
             'create' => Pages\CreateDepartment::route('/create'),
             'edit' => Pages\EditDepartment::route('/{record}/edit'),
         ];
-    }    
+    }
 }
