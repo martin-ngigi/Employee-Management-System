9f2834ffc45d021de9f78f13b725aea1ee2b1348
martin-ngigi <martinwainaina001@gmail.com>
2022-12-12 19:13:26 +0300

    Modified Count Display Widget So that even Country is empty or deleted, code wont crush

--- app/Filament/Resources/EmployeeResource/Widgets/EmployeeStatsOverview.php
+++ app/Filament/Resources/EmployeeResource/Widgets/EmployeeStatsOverview.php
@@ -14,9 +14,18 @@ class EmployeeStatsOverview extends BaseWidget
         $ke = Country::where('country_code', 'KE')->withCount('employees')->first();
         $us = Country::where('country_code', 'US')->withCount('employees')->first();
         return [
+            /**
+             * "$ke? $ke->employees_count : 0" means
+             * if($ke is not null){
+             *   $ke->employees_count
+             * }
+             * else {
+             *   $ke->0
+             * }
+             */
             Card::make('All Employees', Employee::all()->count()),
-            Card::make($ke->name.' Employees', $ke->employees_count),
-            Card::make($us->name.' Employees', $us->employees_count),
+            Card::make('Kenya Employees',$ke? $ke->employees_count : 0),
+            Card::make('US Employees',$us? $us->employees_count: 0),
         ];
     }
 }
