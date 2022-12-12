1da4a1d0688034bb8bb5382b001a50cfb1caeed5
martin-ngigi <martinwainaina001@gmail.com>
2022-12-12 10:41:39 +0300

    Added Table rows

--- database/migrations/2022_12_12_072012_create_employees_table.php
+++ database/migrations/2022_12_12_072012_create_employees_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('country_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('state_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('department_id')->constrained()->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->char('zip_code');
+            $table->date('birth_date');
+            $table->date('date_hired');
             $table->timestamps();
         });
     }
